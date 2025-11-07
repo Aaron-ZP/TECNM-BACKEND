8fe684a4edb29f478dc92d6ffd4c28ca992fad79
Juan Luis <66567588+titan54321@users.noreply.github.com>
2025-11-07 05:28:20 +0000

    Estructura corregida - proyecto corriendo

--- api/src/main/java/mx/tecnm/backend/api/ApiApplication.java
+++ api/src/main/java/mx/tecnm/backend/api/ApiApplication.java
@@ -3,11 +3,9 @@ package mx.tecnm.backend.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "mx.tecnm.backend.api")
 public class ApiApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(ApiApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
 }
