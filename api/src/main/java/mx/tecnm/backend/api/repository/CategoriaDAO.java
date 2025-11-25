4b7f522bf60a4c63c861494dde67be5dd84c0c5a
julianariass <164372731+julianariass@users.noreply.github.com>
2025-11-25 05:26:36 +0000

    cambios y correcciones

--- api/src/main/java/mx/tecnm/backend/api/repository/CategoriaDAO.java
+++ api/src/main/java/mx/tecnm/backend/api/repository/CategoriaDAO.java
@@ -2,17 +2,20 @@ package mx.tecnm.backend.api.repository;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
 public class CategoriaDAO {
-    
+
+    @Autowired
     private JdbcClient jdbcClient;
 
     public List<Categoria> obtenerCategorias() {
         String sql = "SELECT id, nombre FROM categorias";
-        return jdbcClient.sql(sql1)
+        return jdbcClient.sql(sql)
                 .query(new CategoriaRM())
                 .list();
     }
